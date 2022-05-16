d7b1b5361b785099626f8fb0abde01cf11eeba48
Hagen E. Fritz <hagenfritz@gmail.com>
2022-05-16 12:53:31 -0500

    dashboard from AirThings data

--- src/data/make_airthings_dataset.py
+++ src/data/make_airthings_dataset.py
@@ -10,24 +10,27 @@
 # Date: 03/29/2022
 # ----------------------
 
-from importlib.resources import path
 import sys
 import logging
 import pathlib
 import os
-from tracemalloc import start
 
 import pandas as pd
 import numpy as np
 
 from datetime import datetime
 
+logger = logging.getLogger(__name__)
+dir_path = pathlib.Path(__file__).resolve().parent
+logging.basicConfig(filename=f'{dir_path}/data_airthings.log', filemode='w', level=logging.INFO,
+                    format='%(asctime)s: %(name)s (%(lineno)d) - %(levelname)s - %(message)s',datefmt='%m/%d/%y %H:%M:%S')
+
 class Process:
     """
     Processes data from a list of AirThings Devices
     """
 
-    def __init__(self,start_date,end_date,ip_filename="airthings_ips") -> None:
+    def __init__(self,start_date,end_date,ip_filename=None) -> None:
         """
         Parameters
         ----------
@@ -35,7 +38,7 @@ class Process:
             first day to consider for data processing in form "%Y%m%d" (inclusive)
         end_date : str
             last day to consider for data processing in form "%Y%m%d" (inclusive)
-        ip_filename : str, default "airthings_ips"
+        ip_filename : str, None
             filename to find the list of IP addresses
 
         Creates
@@ -61,17 +64,21 @@ class Process:
         self.path_to_this_dir = f"{pathlib.Path(__file__).resolve().parent}"
         self.path_to_data = f"{pathlib.Path(__file__).resolve().parent.parent.parent}/data" # taking advantage of project filesystem
         
-        # logging
-        self.logger = setup_logging() if not logging.getLogger().hasHandlers() else logging.getLogger()
-        self.logger.info('making final data set from raw data')
-        
         # IP Addresses
-        try:
-            ip_file =  open(f"{self.path_to_this_dir}/{ip_filename}.txt")
-            self.ips = ip_file.read().split("\n")
-        except FileNotFoundError:
-            self.logger.exception(f"no {ip_filename}.txt found - please create this file in the same directory")
-            self.ips = []
+        if ip_filename is None:
+            try:
+                meta = pd.read_csv(f"{self.path_to_data}/admin/airthings_meta.csv")
+                self.ips = list(meta["ip_address"].values)
+            except FileNotFoundError:
+                logger.exception("No meta data available, creating empty list:")
+                self.ips = []
+        else: # assuming an str
+            try:
+                ip_file =  open(f"{self.path_to_data}/admin/{ip_filename}.txt")
+                self.ips = ip_file.read().split("\n")
+            except FileNotFoundError:
+                logger.exception(f"no {ip_filename}.txt found - please create this file in the same directory")
+                self.ips = []
 
     def download(self,ip_address,path_to_raw="/home/pi/DATA/"):
         """
@@ -88,8 +95,36 @@ class Process:
         -------
         <void>
         """
-        self.logger.info(f"downloading data from Device {ip_address}")
+        logger.info(f"downloading data from Device {ip_address}")
         os.system(f'scp -r -o ConnectTimeout=3 pi@{ip_address}:{path_to_raw} {self.path_to_data}/interim/')
+
+    def perform_quality_checks(self,data,
+        params=["rh","temperature","co2","voc"]):
+        """
+        Some processing and quality assurance checks
+        
+        Parameters
+        ----------
+        data : DataFrame
+            data in need of processing
+
+        Returns
+        -------
+        data_qc : DataFrame
+            processed data
+        """
+        data_qc = pd.DataFrame()
+        for device in data["device"].unique():
+            data_device = data[data["device"] == device]
+            for param in params:
+                data_device['z'] = abs(data_device[param] - data_device[param].mean()) / data_device[param].std(ddof=0)
+                data_device.loc[data_device['z'] > 2.5, param] = np.nan
+
+            data_device.drop(['z'],axis=1,inplace=True)
+            
+            data_qc = data_qc.append(data_device)
+
+        return data_qc
 
     def make_dataset(self):
         """
@@ -100,17 +135,19 @@ class Process:
         processed : DataFrame
             data from each AirThings Beacon
         """
-        self.logger.info("generating dataset")
+        logger.info("generating dataset")
         if len(self.ips) > 0:
             for ip in self.ips:
                 self.download(ip)
 
             combined = pd.DataFrame()
+            logger.info("Importing files:")
             for file in os.listdir(f"{self.path_to_data}/interim/DATA/"):
                 if file[-1] == "v": # csV files
                     file_date = datetime.strptime(f"{file.split('-')[1]}{file.split('-')[2]}{file.split('-')[3].split('.')[0]}","%Y%m%d")
                     if file_date >= self.start_date and file_date <= self.end_date:
                         device = file.split("-")[0]
+                        logger.info(f"\t{file}")
                         temp = pd.read_csv(f"{self.path_to_data}/interim/DATA/{file}",parse_dates=["timestamp"],infer_datetime_format=True)
                         temp["device"] = device
 
@@ -120,7 +157,7 @@ class Process:
             combined.sort_values(["device","timestamp"],inplace=True)
             combined.set_index("timestamp",inplace=True)
             # saving processed data as class object
-            self.processed = combined
+            self.processed = self.perform_quality_checks(combined)
             # deleting interim data
             os.system(f"rm -r {self.path_to_data}/interim/DATA/")
         else:
@@ -130,13 +167,13 @@ class Process:
         """
         Saves the class dataset to path
         """
-        self.logger.info("saving dataset")
+        logger.info("saving dataset")
         try:
             data_start_date = datetime.strftime(min(self.processed.index).date(),"%Y%m%d")
             data_end_date = datetime.strftime(max(self.processed.index).date(),"%Y%m%d")
             self.processed.to_csv(f"{self.path_to_data}/processed/airthings-data-{data_start_date}-{data_end_date}.csv")
         except AttributeError:
-            self.logger.exception("need to make the dataset first")
+            logger.exception("need to make the dataset first")
 
     def run(self):
         """
@@ -145,25 +182,11 @@ class Process:
         self.make_dataset()
         self.save()
 
-def setup_logging():
-    """
-    Sets up logger
-    """
-    # Create a custom logger
-    logger = logging.getLogger(__name__)
-    dir_path = pathlib.Path(__file__).resolve().parent
-    logging.basicConfig(filename=f'{dir_path}/data_airthings.log', filemode='w', level=logging.INFO,
-                        format='%(asctime)s: %(name)s (%(lineno)d) - %(levelname)s - %(message)s',datefmt='%m/%d/%y %H:%M:%S')
-
-    return logger
-
 if __name__ == '__main__':
     """ 
     Runs data processing scripts to turn raw data downloaded directly from devices into
         cleaned data ready to be analyzed (saved in processed).
     """
-    logger = setup_logging()
-
     # Start date
     try:
         start_date = sys.argv[1]
---
+++ src/data/make_dashboard.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+# ----------------------
+# Make AirThings Dataset
+#
+# Import data from
+# AirThings and
+# PurpleAir, creates
+# figures, and pushes
+# them to Github
+# 
+# Author: Hagen Fritz
+# Date: 05/13/2022
+# ----------------------
+
+from curses import meta
+import sys
+import logging
+import pathlib
+import os
+import argparse
+
+import pandas as pd
+import numpy as np
+
+from datetime import datetime, timedelta
+
+sys.path.append('../../')
+from src.data import make_airthings_dataset, make_purpleair_dataset
+from src.visualization import visualize
+
+logger = logging.getLogger(__name__)
+dir_path = pathlib.Path(__file__).resolve().parent
+logging.basicConfig(filename=f'{dir_path}/dashboard_gen.log', filemode='w', level=logging.INFO,
+                    format='%(asctime)s: %(name)s (%(lineno)d) - %(levelname)s - %(message)s',datefmt='%m/%d/%y %H:%M:%S')
+
+class Data:
+
+    def __init__(self,start_date,end_date,resample_rate) -> None:
+        """
+        Gets data from the AirThings and PurpleAir Devices
+
+        Parameters
+        ----------
+        start_date : str
+            first date to consider in the format of "yyyymmdd"
+        end_date : str
+            final date to soncider in the format of "yyyymmdd"
+        resample_rate : int
+            rate to resample the data to in minutes
+
+        Creates
+        -------
+        start_date : str
+            first date to consider in the format of "yyyymmdd"
+        end_date : str
+            final date to soncider in the format of "yyyymmdd"
+        resample_rate : int
+            rate to resample the data to in minutes
+        """
+        # import paths
+        self.path_to_this_dir = f"{pathlib.Path(__file__).resolve().parent}"
+        self.path_to_data = f"{pathlib.Path(__file__).resolve().parent.parent.parent}/data"
+
+        # dates
+        self.start_date = start_date#datetime.strptime(start_date,"%Y%m%d")
+        self.end_date = end_date#datetime.strptime(end_date,"%Y%m%d")
+
+        self.resample_rate = resample_rate
+
+    def get_airthings(self):
+        """
+        Gets the airthings data
+        """
+        at_processor = make_airthings_dataset.Process(start_date=self.start_date,end_date=self.end_date)
+        at_processor.make_dataset()
+        # reampling
+        data_resampled = pd.DataFrame()
+        for device in at_processor.processed["device"].unique():
+            data_device = at_processor.processed[at_processor.processed["device"] == device]
+            data_device_resampled = data_device.resample(f"{self.resample_rate}T").mean()
+            
+            data_device_resampled["device"] = device
+            data_resampled = data_resampled.append(data_device_resampled)
+
+        # adding in meta data
+        meta_at = pd.read_csv(f"{self.path_to_data}/admin/airthings_meta.csv")
+        # converting both id columns to numerical
+        meta_at["mac_address"] = pd.to_numeric(meta_at["mac_address"])
+        data_resampled["device"] = pd.to_numeric(data_resampled["device"])
+        data_resampled.to_csv(f"{self.path_to_data}/interim/airthings-dashboard-test.csv")
+        self.data_at = data_resampled.merge(right=meta_at,left_on=["device"],right_on=["mac_address"],how="left")
+        self.data_at["timestamp"] = data_resampled.index
+        self.data_at.set_index("timestamp",inplace=True)
+        self.data_at.to_csv(f"{self.path_to_data}/interim/airthings-dashboard.csv")
+
+    def get_purpleair(self):
+        """
+        Gets the purpleair data
+        """
+        pass
+
+    def merge_data(self):
+        """
+        Merges data from both devices
+        """
+        try:
+            self.data_at.reset_index(inplace=True)
+            self.data_pa.reset_index(inplace=True)
+            self.data = self.data_at.merge(right=self.data_pa,on=["kit_no","timestamp"],how="outter")
+        except AttributeError:
+            logger.exception("Missing processed data:")
+            # saving the aggregate data as either AirThings or Purple Air - whichever is available
+            try:
+                logger.warning("Aggregate data from AirThings ONLY")
+                self.data = self.data_at.set_index("timestamp")
+            except AttributeError: # no AirThings data
+                logger.warning("Aggregate data from PurpleAir ONLY")
+                self.data = self.data_pa.set_index("timestamp")
+
+        self.data.to_csv(f"{self.path_to_data}/interim/data-dashboard.csv")
+
+    def run(self):
+        """
+        Runs the data ingress process
+        """
+        self.get_airthings()
+        #self.get_purpleair()
+        self.merge_data()
+
+class Figures:
+
+    def __init__(self) -> None:
+        """
+        Creates the figures from the provided data
+        """
+        # import paths
+        self.path_to_this_dir = f"{pathlib.Path(__file__).resolve().parent}"
+        self.path_to_data = f"{pathlib.Path(__file__).resolve().parent.parent.parent}/data"
+
+        self.data = pd.read_csv(f"{self.path_to_data}/interim/data-dashboard.csv",
+            index_col=0,parse_dates=True,infer_datetime_format=True)
+        
+    def generate_timeseries(self,params=["co2","voc","temperature","rh"]):
+        """
+        Generates the timeseries figures
+
+        Parameters
+        ----------
+        params : list of str
+            parameters to generate timeseries for
+        """
+        ts_gen = visualize.Dashboard()
+        for param in params:
+            ts_gen.timeseries_aggregate(self.data,param=param,show=False,save=True,subdir="/dashboard")
+
+    def generate_heatmaps(self,params=["co2","voc","temperature","rh"]):
+        """
+        Generates the timeseries figures
+
+        Parameters
+        ----------
+        params : list of str
+            parameters to generate timeseries for
+        """
+        ts_gen = visualize.Dashboard()
+        for param in params:
+            for device in self.data["kit_no"].unique():
+                ts_gen.heatmap_individual(self.data,id_col="kit_no",device=device,param=param,show=False,save=True,subdir="/dashboard")
+
+
+def main(period=60,resample_rate=15):
+    """
+    Ingresses the data, generates the figures, and pushes them to Github
+
+    Parameters
+    ----------
+    period : int, default 60
+        number of days to plot
+    resample_rate : int, default 15
+        frequency to resample data to in minutes
+    """
+    # Ingressing
+    # ----------
+    ## Getting the import dates
+    end_date = datetime.now()
+    end_date_str = datetime.strftime(end_date,"%Y%m%d")
+    start_date = end_date - timedelta(days=period)
+    start_date_str = datetime.strftime(start_date,"%Y%m%d")
+    ## generating and saving datasets
+    ingress = Data(start_date=start_date_str,end_date=end_date_str,resample_rate=resample_rate)
+    ingress.run()
+    # Figure Gen
+    # ----------
+    vis = Figures()
+    ## timeseries figures
+    vis.generate_timeseries()
+    vis.generate_heatmaps()
+    # Pushing to GH
+    # -------------
+    path_to_push = f"{pathlib.Path(__file__).resolve().parent.parent.parent}/reports/figures/dashboard/"
+    current_time = datetime.strftime(datetime.now(),"%m/%d %H:%M")
+    try:
+        os.system(f'git add {path_to_push}* && git commit -m "figure update {current_time}" && git push')
+    except Exception as e:
+        logger.exception("Error:")
+
+if __name__ == '__main__':
+    """ 
+    Generates the figures used in the dashboard
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', help="number of days of data to show", default=60, type=int)
+    parser.add_argument('-r', help="resample rate in minutes", default=15, type=int)
+    args = parser.parse_args()
+
+    main(period=args.d,resample_rate=args.r)
