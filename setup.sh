3d65a3a4a76f30c4f48c42997935a951bdbcf67d
Hagen Fritz <33231914+HagenFritz@users.noreply.github.com>
2022-05-16 13:10:55 -0500

    created

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update package list
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Additional apt packages
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-venv -y
+sudo apt-get install libatlas-base-dev -y #for numpy
+
+# Github Credentials
+git config pull.rebase false
+
+# Set up locale, timezone, language
+sudo timedatectl set-timezone US/Central
+
+# Virtual Environment Setup
+rm -rf ~/bleed-orange-measure-iaq/.venv # remove if it already exists
+mkdir ~/bleed-orange-measure-iaq/.venv
+python3 -m venv ~/bleed-orange-measure-iaq/.venv
+
+# Dashboard update script
+sudo /bin/bash -c 'echo "*/15 * * * * root sh /home/pi/bleed-orange-measure-iaq/udpate_figures.sh" >> /etc/crontab'
